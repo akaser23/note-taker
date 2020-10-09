3ebf408369fb9f4a706808640536148195bfbed4
Anna Kaser <kaserann96@gmail.com>
2020-10-08 19:02:35 -0600

    get and api/get requests

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 const express = require('express');
+const PORT = process.env.PORT || 3003;
+const app = express();
+const path = require('path');
+const { notes } = require('./data/db');
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const animal = createNewNote(req.body, notes);
+    res.json(notes);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
