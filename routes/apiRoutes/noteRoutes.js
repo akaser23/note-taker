1061ece522c2e49a1f6a3ae2d524e51ee0bfbaef
Anna Kaser <kaserann96@gmail.com>
2020-10-10 13:22:04 -0600

    lib and routes folders added

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { createNewNote } = require('../../lib/notes');
+const { notes } = require('../../data/db.json');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const PORT = process.env.PORT || 3003;
 const app = express();
-const path = require('path');
-const { notes } = require('./data/db');
 
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './data/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-};
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
+app.use(express.urlencoded({encoded: true}));
+app.use(express.json());
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-    const animal = createNewNote(req.body, notes);
-    res.json(notes);
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
